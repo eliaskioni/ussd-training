3e9b96d36b537d825129f66f09b336e47c80116c
EliasKioni <eliaskioni94@gmail.com>
2019-10-22 11:42:14 +0200

    INITIAL COMMIT

---
+++ core/views.py
@@ -0,0 +1,19 @@
+from ussd.core import UssdView, UssdRequest,_customer_journey_files
+
+
+class USSDGateway(UssdView):
+    path = os.path.dirname(os.path.abspath(__file__))
+    customer_journey_conf = path + "/equity_bank_kenya_customer_journey.yml"
+
+    def post(self, req):
+        session_id = req.data.get('session_id')
+        user_input = req.data.get('text')
+        ussd_request = UssdRequest(
+            phone_number=req.data['phoneNumber'].strip('+'),
+            session_id=session_id,
+            ussd_input=user_input,
+            service_code=req.data['serviceCode'],
+            language=req.data.get('language', 'en')
+        )
+
+        return ussd_request
